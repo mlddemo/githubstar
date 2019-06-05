2b929c2195c728fb9e665bc1dba37d8e72099164
mlddemo <mlddemo@outlook.com>
2019-06-05 22:14:26 +0100

    Routing

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createBrowserHistory } from 'history'
+import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from './rootReducer'
+import createRootReducer from './rootReducer'
+import { routerMiddleware } from 'connected-react-router';
+
+export const history = createBrowserHistory()
 
 export default function configureStore() {
     return createStore(
-        rootReducer,
-        applyMiddleware(thunk)
+        createRootReducer(history),
+        compose(
+            applyMiddleware(
+                routerMiddleware(history),
+                thunk)
+        )
     )
 }
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
 import dummy from './reducers/dummy'
 
-export default combineReducers({
+export default history => combineReducers({
+    router: connectRouter(history),
     dummy
 })
